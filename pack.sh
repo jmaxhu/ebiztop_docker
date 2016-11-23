4eb8311a38be05deef1518df6d3e3c762c6e5003
maxwell <maxwell_hu@163.com>
2016-11-23 17:18:47 +0800

    modify pack.sh improve path

--- pack.sh
+++ pack.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-tar czvf eBizTop.tar.gz --exclude 'eBizTop/web/node_modules' --exclude 'eBizTop/web/storage/logs' --exclude 'eBizTop/web/public/ebiztop.apk' --exclude 'eBizTop/web/public/images/temp' --exclude 'eBizTop/web/.idea' --exclude 'eBizTop/web/.vscode' --exclude 'eBizTop/ui' --exclude 'eBizTop/app' --exclude 'eBizTop/.git' --exclude 'eBizTop/worker.log' /www/eBizTop
+cd /www
+
+tar czf eBizTop.tar.gz --exclude 'eBizTop/web/node_modules' --exclude 'eBizTop/web/storage/logs' --exclude 'eBizTop/web/public/ebiztop.apk' --exclude 'eBizTop/web/public/images/temp' --exclude 'eBizTop/web/.idea' --exclude 'eBizTop/web/.vscode' --exclude 'eBizTop/ui' --exclude 'eBizTop/app' --exclude 'eBizTop/.git' --exclude 'eBizTop/worker.log' eBizTop
