62133a89803c3bcfd8355a26e391b7e93a112846
maxwell <maxwell_hu@163.com>
2016-11-29 16:51:01 +0800

    phantomjs改成下载安装。

--- provision.sh
+++ provision.sh
@@ -159,12 +159,11 @@ apt-get install -y nodejs
 /usr/bin/npm install -g pm2
 
 # Install phantomjs
-cd /
-tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2
-mv phantomjs-2.1.1-linux-x86_64 /usr/local/share
-ln -sf /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
-
-rm -rf phantomjs-2.1.1-linux-x86_64.tar.bz2
+export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
+wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
+tar xvjf $PHANTOM_JS.tar.bz2
+mv $PHANTOM_JS /usr/local/share
+ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
 
 # Install simsun font
 chmod 644 /usr/share/fonts/simsun.ttc
